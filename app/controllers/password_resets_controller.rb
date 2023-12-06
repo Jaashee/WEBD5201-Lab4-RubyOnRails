3ce83b0cbb508bbdb85751ca234ee78388973d56
Jaashee <jashakan2000@gmail.com>
2023-12-06 00:37:06 -0500

    Complete assigment up-to part 22. Code broke while working on part 18(accessing the current user) still completed the rest of the parts.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -12,4 +12,25 @@ class PasswordResetController < ApplicationController
     redirect_to root_path, notice: "If an account with that email was found, we have sent a link to reset you password"
   end
 
+  def edit
+    @user = User.find_signed!(params[:tokens], purpose: "password_reset")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: "Your token has expired. Please try again"
+  end
+
+  def update
+    @user = User.find_signed!(params[:tokens], purpose: "password_reset")
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: "Your passwords was reset sucessfully. Please sign in."
+    else
+      render :edit, status: :unprocessable_entity
+
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 end
